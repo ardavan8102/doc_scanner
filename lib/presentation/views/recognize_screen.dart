8e50b4cdcc15f117969e15d3946d63ee441403ea
ardavan8102 <eskandariardavan81@gmail.com>
2026-06-20 17:37:32 +0330

    ml kit added to image recognizer

--- lib/presentation/views/recognize_screen.dart
+++ lib/presentation/views/recognize_screen.dart
@@ -1,17 +1,51 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
 class RecognizeScreen extends StatefulWidget {
-  File? image;
+  final File image;
 
-  RecognizeScreen({super.key, this.image});
+  const RecognizeScreen({super.key, required this.image});
 
   @override
   State<RecognizeScreen> createState() => _RecognizeScreenState();
 }
 
 class _RecognizeScreenState extends State<RecognizeScreen> {
+
+  late TextRecognizer textRecognizer;
+
+  @override
+  void initState() {
+    super.initState();
+    textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+    doTextRecognation();
+  }
+  
+
+  dynamic doTextRecognation() async {
+    InputImage inputImage = InputImage.fromFile(widget.image);
+
+    final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
+
+    String text = recognizedText.text;
+    for (TextBlock block in recognizedText.blocks) {
+      final Rect rect = block.boundingBox;
+      final List<Point<int>> cornerPoints = block.cornerPoints;
+      final String text = block.text;
+      final List<String> languages = block.recognizedLanguages;
+
+      for (TextLine line in block.lines) {
+        // Same getters as TextBlock
+        for (TextElement element in line.elements) {
+          // Same getters as TextBlock
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +59,7 @@ class _RecognizeScreenState extends State<RecognizeScreen> {
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
-              image: DecorationImage(image: FileImage(widget.image!))
+              image: DecorationImage(image: FileImage(widget.image))
             ),
           ),
         ],
